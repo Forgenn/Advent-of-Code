3500b9a260c8d94f12bc965efdb57edea8bcaa10
Forgenn <ipolmonxammar@gmail.com>
2021-12-06 19:21:13 +0100

    Day 6 Update

--- 2021/Day 6/main.py
+++ 2021/Day 6/main.py
@@ -1,22 +1,15 @@
 def solve(days):
     with open("input.txt") as input:
         lanternfish_school = [int(i) for i in input.readline().split(",")]
-        lanternfish_dict = {}
+        lanternfish_dict = [0] * 9
         aux_lanterfish_6 = 0
         aux_lanterfish_8 = 0
 
-        for i in range(9):
-            lanternfish_dict[i] = 0
-
         for fish in lanternfish_school:
-            if fish in lanternfish_dict:
-                lanternfish_dict[fish] += 1
-            else:
-                lanternfish_dict[fish] = 1
-
+            lanternfish_dict[fish] += 1
 
         for i in range(days):
-            for key, value in lanternfish_dict.items():
+            for key, value in enumerate(lanternfish_dict):
                 if key == 0 and value != 0:
                     aux_lanterfish_8 = value
                     aux_lanterfish_6 = value
@@ -29,7 +22,8 @@ def solve(days):
             lanternfish_dict[6] += aux_lanterfish_8
             aux_lanterfish_8 = 0
             aux_lanterfish_6 = 0
-        return sum(lanternfish_dict.values())
+
+        return sum(lanternfish_dict)
 
 
 
